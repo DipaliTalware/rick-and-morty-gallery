7813827dbd00177d8832e9d94fe58f88549b24f3
sama-hh <samahajimuradovah@gmail.com>
2024-10-08 17:03:16 +0200

    bonus

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     const [characters, setCharacters] = useState<characterType[]>(data);
     const [filteredCharacters, setFilteredCharacters] = useState<characterType[]>([]);
     const [error, setError] = useState<boolean>(false);
-
+    const [count, setCount] = useState<number>(0); // Track current index for pagination
 
     useEffect(() => {
         const filteredCharacters = characters.filter(character => character.name.toLowerCase().includes(search.toLowerCase()));
@@ -21,11 +21,25 @@ function App() {
         setFilteredCharacters(filteredCharacters);
     }, [search, characters]);
 
+    const showFiveCharacters = () => {
+        setCount(prev => prev + 5);
+    }
+
+    const printCharactersByCount = filteredCharacters.slice(count, count + 5).map(character => {
+        return (
+            <div key={character.id}>
+                <p>{character.name}, {character.status}, {character.species}, {character.gender}</p>
+            </div>
+        )
+    });
+
     return (
         <>
             <input type="text" name="search" value={search} onChange={(e) => setSearch(e.target.value)}
                    placeholder="Search..."/>
             {error? <div>"Nothing found"</div> :<RickMortyGallery characters={filteredCharacters}/>}
+            <button onClick={() => showFiveCharacters()}>show 5 characters</button>
+            {printCharactersByCount}
         </>
     )
 }
